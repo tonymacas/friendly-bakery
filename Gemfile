8315d5c9b690a7bcde55182e25d72f8a30cb7998
Tony Mac <tonymac@Tony.local>
2018-04-26 10:23:32 -0400

    First commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org" do
+gem 'sinatra'
+gem 'sendgrid-api'
+gem 'materialize-sass'
+end
---
+++ app.rb
@@ -0,0 +1,24 @@
+require 'sinatra'
+require 'sendgrid-ruby'
+require 'materialize-sass'
+
+
+get '/' do
+erb :index
+end
+
+# post "/" do
+
+# end
+
+get '/cookies/' do
+    erb :cookies
+end
+
+get '/cakes/' do
+erb :cakes
+end
+
+get '/muffins/' do
+erb :muffins
+end
